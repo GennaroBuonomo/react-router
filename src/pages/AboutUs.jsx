2759a6aeb517e4082cd44b165b6ab7d87eb93f88
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-08 15:43:14 +0200

    Creazione del contenuto delle pagine

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2>AboutUs</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+   <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2>ProductsPage</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsPage
