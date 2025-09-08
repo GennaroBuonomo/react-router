8b29b9151cb17d843390c5c08b914eb93d1570b6
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-08 16:56:28 +0200

    creazione cartella components e inizio l header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+  <header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <ul className="list-unstyled d-flex">
+            <li className="me-3">
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+  )
+}
+
+export default Header
