974517f335214dfcc435e08c09aff2186fd343f2
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-08 17:41:02 +0200

    creazione della nuova pagina e importazzione di esso

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import ProductsPage from "./pages/ProductsPage";
+import ProductsDetailsPage from './pages/ProductsDetailsPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/About-us" element={<AboutUs />} />
       <Route path="/Products" element={<ProductsPage />} />
+      <Route path=":id" element={<ProductsDetailsPage />} />      
     </Routes>
    </BrowserRouter>
   )
---
+++ src/pages/ProductsDetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsDetailsPage = () => {
+  return (
+    <div>
+      <h1>Dettaglio prodotto</h1>
+    </div>
+  )
+}
+
+export default ProductsDetailsPage
